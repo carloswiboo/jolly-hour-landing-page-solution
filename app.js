e0d1f66b42f6912b662ab3d9a044b473d1c82fb4
Carlos Francisco Estrada Garcidueñas <carlos@wiboo.com.mx>
2022-03-27 16:16:41 -0600

    emparejando cambios

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ function getUrlAppJollyHour() {
       "toolbar=0,location=0,menubar=0"
     );
   if (navigator.appVersion.indexOf("Mac") != -1)
-    window.open(
-      "https://apps.apple.com/mx/app/jolly-hour/id1605882108",
-      "_blank",
-      "toolbar=0,location=0,menubar=0"
-    );
+      window.open(
+        "https://apps.apple.com/mx/app/jolly-hour/id1605882108",
+        "_blank",
+        "toolbar=0,location=0,menubar=0"
+      );
   if (navigator.appVersion.indexOf("X11") != -1)
     window.open(
       "https://play.google.com/store/apps/details?id=com.carloswiboo.jollyhourmobileapp&hl=es_MX",
